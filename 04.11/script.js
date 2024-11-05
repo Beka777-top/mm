066d301fecaf1ccf361eb44bfbccd89b702eec8e
Troe <20bekarys04@gmail.com>
2024-11-05 14:32:41 +0500

    04.11

---
+++ 04.11/script.js
@@ -0,0 +1,22 @@
+var age = 16;
+console.log( age > 16);
+
+if((age %2==0)){
+    console.log(true)
+} else{ 
+    console.log(false)
+}
+
+
+var temperature = 20;
+console.log((temperature >= 20 && temperature <= 30));
+
+var num = 4;
+console.log(num >= 0);
+
+var b = 50; 
+if (b % 5 === 0 && b % 10 === 0) {
+    console.log("шын");
+} else {
+    console.log("жалған");
+}
