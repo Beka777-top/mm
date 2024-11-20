8085f41411ef01b4a3822fa3b693b315747eef5e
Troe <20bekarys04@gmail.com>
2024-11-20 21:40:13 +0500

    20.11hw

--- 20.11hw/script.js
+++ 20.11hw/script.js
@@ -1,16 +1,19 @@
-var totalDistance =0;
-while(true){
-    var distance = +prompt("kashictic engiz");
-    if (distance == 0){
+var totalDistance = 0;
+const Find = () =>{
+    while (true) {
+      var distance = +prompt("kashictic engiz");
+      if (distance == 0) {
         break;
-    }
-    var unit = prompt("olshem birlic engiz m, dm, km");
-    if (unit == "m"){
+      }
+      var unit = prompt("olshem birlic engiz m, dm, km");
+      if (unit == "m") {
         totalDistance += distance;
-    }else if (unit == "dm"){
-        totalDistance += distance /10;
-    }else if (unit == "km"){
+      } else if (unit == "dm") {
+        totalDistance += distance / 10;
+      } else if (unit == "km") {
         totalDistance += distance * 1000;
-    } 
+      }
+    }
+    return totalDistance
 }
-console.log( totalDistance);
+console.log(Find());
