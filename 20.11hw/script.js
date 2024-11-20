1782801877fc47ac5957cd24e02bf8144a7a8a20
Troe <20bekarys04@gmail.com>
2024-11-20 21:29:16 +0500

    20.11hw

---
+++ 20.11hw/script.js
@@ -0,0 +1,16 @@
+var totalDistance =0;
+while(true){
+    var distance = +prompt("kashictic engiz");
+    if (distance == 0){
+        break;
+    }
+    var unit = prompt("olshem birlic engiz m, dm, km");
+    if (unit == "m"){
+        totalDistance += distance;
+    }else if (unit == "dm"){
+        totalDistance += distance /10;
+    }else if (unit == "km"){
+        totalDistance += distance * 1000;
+    } 
+}
+console.log( totalDistance);
