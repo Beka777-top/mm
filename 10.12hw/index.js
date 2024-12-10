bae2c54754e897d5a58af47323bcc6ce6fac9d8e
Troe <20bekarys04@gmail.com>
2024-12-10 22:19:13 +0500

    10.12hw

---
+++ 10.12hw/index.js
@@ -0,0 +1,8 @@
+const newTitle =document.querySelector(".title");
+newTitle.textContent = "Hello my firends! welcome"
+
+const newInto = document.querySelector(".into");
+newInto.textContent ="JavaScript is a multi-paradigm, dynamic language with types and operators, standard built-in objects, and methods. Its syntax is based on the Java and C languages — many structures from those languages apply to JavaScript as well. JavaScript supports object-oriented programming with object prototypes and classes."
+
+const newFact = document.querySelector(".fact");
+newFact.textContent = "The real name is not “Javascript”, but ECMAScript. It was born in 1995 and was created in 10 days by the programmer Brendan Eich."
